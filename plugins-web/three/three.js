3af271fcae5df97800bb5f53ed9217e08ab761b4
Jon Swenson <jswenson74@gmail.com>
2015-02-12 12:18:19 -0500

    Lesson-5  Static files (js/css) & bower install
    1. Configure hapi to include static files.
    2. Install bower.
    3. Configure html5.html page to include jQuery and
       project JavaScript page.
    
    1. Configure hapi to include static files.
    In plugins/web/three/three.js configured include static files settings.
    The view at plugins/web/three/views/html5.html includes
    the static css file ./public/assets/styles.css.
    
    2. Install bower
    npm install bower -g
    configured .bowerrc to install bower componentes at:
    ./public/bower_components
    View .bowerrc for example.
    bower install jquery --save
    Installs jquery in public/bower_components directory.
    Now, the project is configured to easily add angular to it.
    
    3. Configure html5.html to include jQuery and JS script.
    Sample view:
    plugins/web/three/views/html5.html.
    This page includes jQuery from:
    ./public/bower_components/jquery/dist/.
    Plus, plugins/web/three/views/html5.html includes assets/js/html5.js
    file and prints to console when the page is ready. This illustrates
    loading jQuery and your own JavaScript files.

--- plugins-web/three/three.js
+++ plugins-web/three/three.js
@@ -1,40 +1,67 @@
-
-exports.register = function(server,options,next){
-
-	server.views({
-		engines:{
-			html:require('handlebars')				
-		},	
-		relativeTo: __dirname,
-		path: './views',
-		helpersPath:'./views/helpers'
-	});
-
-	server.route({
-		method:'GET',
-		path:'/',
-		handler:function(request, reply){
-			// html view 
-			reply.view('index');
-		}
-	});
-
-	server.route({
-		method:'GET',
-		path:'/hapi',
-		handler:function(request, reply){
-			// html view 
-			reply.view('hapi');
-		}
-	});
+exports.register = function(server, options, next) {
 
 
-	next();
+   server.views({
+      engines: {
+         html: require('handlebars')
+      },
+      relativeTo: __dirname,
+      path: './views',
+      helpersPath: './views/helpers'
+   });
+
+
+	 /*
+		* CONFIG STATIC FILES INCLUDE 
+		*/
+   server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+         directory: {
+            path: 'public',
+            // listing: true d/n show diretory tree. 
+         }
+      }
+   });
+
+
+
+   server.route({
+      method: 'GET',
+      path: '/',
+      handler: function(request, reply) {
+         // html view 
+         reply.view('index');
+      }
+   });
+
+
+   server.route({
+      method: 'GET',
+      path: '/hapi',
+      handler: function(request, reply) {
+         // html view 
+         reply.view('hapi');
+      }
+   });
+
+
+   server.route({
+      method: 'GET',
+      path: '/html5',
+      handler: function(request, reply) {
+         // html view 
+         reply.view('html5');
+      }
+   });
+
+   next();
 
 };
 
 exports.register.attributes = {
-	name:'three',
-	version:'0.0.1',
-	pkg:require('./package.json')
+   name: 'three',
+   version: '0.0.1',
+   pkg: require('./package.json')
 };
